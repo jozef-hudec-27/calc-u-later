6c46bf42abf09640e460a1bf2315b6e1433bd2ca
Jozef Hudec <jojohudec27@gmail.com>
2022-08-15 12:54:06 +0200

    Fix bugs + show result so far

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ let displayString = ''
 let calculatedSoFar = 0
 let clickedEquals = false
 
-const currentInput = document.querySelector('#calc-display > p')
+const currentInput = document.querySelector('#calc-display > .current-display-input')
+const prevDisplayResult = document.querySelector('#calc-display > .prev-display-result')
 
 Array.from(keys).forEach(key => {
     key.addEventListener('click', e => {
@@ -20,21 +21,12 @@ Array.from(keys).forEach(key => {
         if ('0123456789,'.includes(keyVal)) {
             displayString += key.textContent   
             currentInput.textContent = displayString 
-        } else if (OPERATORS.includes(keyVal) && displayString) {
+        } else if (OPERATORS.includes(keyVal) && (displayString || prevDisplayResult.textContent)) {
             let prevDisplayString = displayString.slice()
 
-            switch(prevDisplayString[0]) {
-                case '−':
-                    calculatedSoFar -= getNumberFromString(prevDisplayString)
-                    break
-                case '×':
-                    calculatedSoFar *= getNumberFromString(prevDisplayString)
-                    break
-                case '÷':
-                    calculatedSoFar /= getNumberFromString(prevDisplayString)
-                    break
-                default:
-                    calculatedSoFar += getNumberFromString(prevDisplayString)
+            if (!(prevDisplayString.length === 1 && OPERATORS.includes(prevDisplayString))) {
+                calculateSoFar(prevDisplayString)
+                prevDisplayResult.textContent = calculatedSoFar
             }
 
             displayString = keyVal
@@ -44,8 +36,9 @@ Array.from(keys).forEach(key => {
                 case 'AC':
                     calculatedSoFar = 0
                     displayString = ''
-                    currentInput.textContent = ''
+                    currentInput.textContent = displayString
                     clickedEquals = false
+                    prevDisplayResult.textContent = ''
                     break
                 case '⇚':
                     displayString = displayString.slice(0, displayString.length-1)
@@ -55,19 +48,10 @@ Array.from(keys).forEach(key => {
                     break
                 case '=':
                     clickedEquals = true
+                    prevDisplayResult.textContent = ''
 
-                    switch(displayString[0]) {
-                        case '−':
-                            calculatedSoFar -= getNumberFromString(displayString)
-                            break
-                        case '×':
-                            calculatedSoFar *= getNumberFromString(displayString)
-                            break
-                        case '÷':
-                            calculatedSoFar /= getNumberFromString(displayString)
-                            break
-                        default:
-                            calculatedSoFar += getNumberFromString(displayString)
+                    if (!(displayString.length === 1 && OPERATORS.includes(displayString))) {
+                        calculateSoFar(displayString)
                     }
                     
                     displayString = calculatedSoFar
@@ -84,3 +68,20 @@ function getNumberFromString(numStr) {
     return OPERATORS.includes(numStr[0]) ? +numStr.slice(1) : +numStr
 }
 
+function calculateSoFar(str) { // we calculate new result only when we click '=' or any operator
+    const num = getNumberFromString(str)
+
+    switch(str[0]) {
+        case '−':
+            calculatedSoFar -= num
+            break
+        case '×':
+            calculatedSoFar *= num
+            break
+        case '÷':
+            calculatedSoFar /= num
+            break
+        default:
+            calculatedSoFar += num
+    }
+}
